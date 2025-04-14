eae20286a6dcecafcddf60c7c7f4eca5c2acba2a
ElizabethBenitez20 <141886983+ElizabethBenitez20@users.noreply.github.com>
2025-04-14 14:06:46 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to dictionary app!</h1>
+      <button className="btn btn-primary">Hello</button>
+    </div>
+  );
+}
+
+export default App;
