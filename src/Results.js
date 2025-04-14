5540cf743487420cd648e2f5764ccb06e0233f51
ElizabethBenitez20 <141886983+ElizabethBenitez20@users.noreply.github.com>
2025-04-14 19:47:24 -0300

    Rendered the word meanings

---
+++ src/Results.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Meaning from "./Meaning";
+
+export default function Results(props) {
+  if (props.results) {
+    return (
+      <div className="Results">
+        <h2>{props.results.word}</h2>
+        <h3>{props.results.phonetic}</h3>
+        {props.results.meanings.map((meaning, index) => (
+          <div key={index}>
+            <Meaning meaning={meaning} />
+          </div>
+        ))}
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
